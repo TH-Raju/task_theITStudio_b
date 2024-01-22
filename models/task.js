d2744b74044d68d0d9be0a01b6ad1ec8c9bb4086
Tofajjol Hosen Raju <rajukhan894200@gmail.com>
2024-01-22 12:01:05 +0600

    backend update

--- models/task.js
+++ models/task.js
@@ -1,26 +1,20 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
-let taskSchema = new Schema(
-  {
-    userId: {
-      type: String,
-    },
-    time: {
-      type: String,
-    },
-    date: {
-      type: String,
-    },
-    title: {
-      type: String,
-    },
-    description: {
-      type: String,
-    },
+let taskSchema = new Schema({
+  name: {
+    type: String,
   },
-  { timestamps: true }
-);
+  phone: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  hobbies: {
+    type: String,
+  },
+});
 
 let Task = mongoose.model("task", taskSchema);
 module.exports = Task;
